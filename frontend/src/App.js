aabffb568cd011fcf4d4195f652d563aef533441
FIRST_NAME LAST_NAME <vigneshmsmg2000@gmail.com>
2020-07-09 16:15:09 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,11 @@ import './App.css';
 
 import HR_page from "./components/layouts/HR_page";
 import Logout from "./components/layouts/Logout";
+import Add from "./components/layouts/Add";
+import Delete from "./components/layouts/Delete";
+import Update from "./components/layouts/Update";
+import Info from "./components/layouts/Info";
+import Options from "./components/layouts/Options";
 class App extends React.Component{
 render(){
 
@@ -16,6 +21,11 @@ render(){
          <Route exact path="/" component={LoginForm}/>
          <Route path="/HR_page" component={HR_page}/>
          <Route path="/logout" component={Logout}/>
+         <Route path="/Add" component={Add}/>
+         <Route path="/Delete" component={Delete}/>
+         <Route path="/Update" component={Update}/>
+         <Route path="/Info" component={Info}/>
+         <Route path="/Options" component={Options}/>
           
          </Switch>
         );
