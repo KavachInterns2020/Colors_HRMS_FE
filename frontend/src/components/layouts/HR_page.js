4801192256a7a61dcd6afce0249d3a3cddac9e3d
rajkumar <rajkumarbaba12062000@gmail.com>
2020-07-10 12:26:51 +0530

    useless

--- frontend/src/components/layouts/HR_page.js
+++ frontend/src/components/layouts/HR_page.js
@@ -1,101 +1,103 @@
-import React, { Component } from 'react';
-import {Link,Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import timesheet1 from "../../timesheet1.jpg";
 import performance from "../../performance.jpg";
 import crud from "../../crud.png";
 
-
 export default class HR_page extends Component {
-    constructor(props){
-        super(props)
-        const token = localStorage.getItem("token")
+  constructor(props) {
+    super(props);
+    const token = localStorage.getItem("token");
 
-
-        let loggedIn =true
-        if(token == null){
-            loggedIn=false
-        }
-
-        this.state={
-            loggedIn
-        }
-
-
+    let loggedIn = true;
+    if (token == null) {
+      loggedIn = false;
     }
-    render() {
-       if(this.state.loggedIn===false){
-           return<Redirect to="/"/>
-       }
 
+    this.state = {
+      loggedIn,
+    };
+  }
+  render() {
+    if (this.state.loggedIn === false) {
+      return <Redirect to="/" />;
+    }
 
+    return (
+      <div>
+        <Navbar />
 
-        return (
-            <div>
-                <Navbar/>
-                
-                
-                <div className="app">
-              
-              <Link to="/logout">Logout</Link>
-              <div class="container">
-        <div class="card">
-            <Link to="/Options">
-            <div class="face face1" >
-                <div class="content">
-                <img  src={crud} alt="logo"/>
+        <div className="app">
+          <Link to="/logout">Logout</Link>
+          <div class="container">
+            <div class="card">
+              <Link to="/Options">
+                <div class="face face1">
+                  <div class="content">
+                    <img src={crud} alt="logo" />
                     <h3>Crud operation</h3>
+                  </div>
                 </div>
-            </div>
-            </Link>
-            
-            <div class="face face2">
+              </Link>
+
+              <div class="face face2">
                 <div class="content">
-                <p style={{color:"black" }}><small style={{fontSize:"20px"}}>
-                    This feature is for adding deleting updating employee details</small></p>
-                        <a href="#">Read More</a>
+                  <p style={{ color: "black" }}>
+                    <small style={{ fontSize: "20px" }}>
+                      This feature is for adding deleting updating employee
+                      details
+                    </small>
+                  </p>
+                  <a href="#">Read More</a>
                 </div>
+              </div>
             </div>
+            <div class="card">
+              <div class="face face1">
+                <div class="content">
+                  <img src={performance} alt="logo" />
+                  <h3>Performance Analysis</h3>
+                </div>
+              </div>
+              <div class="face face2">
+                <div class="content">
+                  <p style={{ color: "black" }}>
+                    <small style={{ fontSize: "20px" }}>
+                      This feature is for analysing the performance of an
+                      employee
+                    </small>
+                  </p>
+
+                  <a href="#">Read More</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="face face1">
+                <div class="content">
+                  <img src={timesheet1} alt="logo" />
+                  <h3>Emp Management</h3>
+                </div>
+              </div>
+              <div class="face face2">
+                <div class="content">
+                  <p style={{ color: "black" }}>
+                    <small style={{ fontSize: "20px" }}>
+                      This feature is for manageing the employee details in a
+                      structured manner
+                    </small>
+                  </p>
+                  <a href="#">Read More</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <img  src={performance} alt="logo"/>
-                    <h3>Performance Analysis</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                <p style={{color:"black" }}><small style={{fontSize:"20px"}}>
-                    This feature is for analysing the performance of an employee</small></p>
-                    
-                        <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <img  src={timesheet1} alt="logo"/>
-                    <h3>Emp Management</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                    <p style={{color:"black" }}><small style={{fontSize:"20px"}}>
-                    This feature is for manageing the employee details in a structured manner</small></p>
-                        <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-    </div>
-                
-              
-                </div>
-                
-                <Footer/>
-            </div>
-        )
-    }
+
+        <Footer />
+      </div>
+    );
+  }
 }
--- frontend/src/components/layouts/Info.js
+++ frontend/src/components/layouts/Info.js
@@ -1,68 +1,96 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import {Link,Redirect} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 export default class Info extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar/>
-                   <div className="app" style={{backgroundColor:"white",color:"black",padding:"30px",marginBottom:"40px"}}>
-                   <Link to="/logout">Logout</Link>
-<div className="topnav">
-<input type="text" placeholder="Search.." style={{marginBottom:"4px"}}></input>
-</div>
-                   
-                   <table id="customers">
-                   
-  <tr>
-    <td>Authentication ID</td>
-    <td><input type="text"></input></td>
-    
-  </tr>
-  <tr>
-  <td>Employee ID</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>First Name</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Last Name</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Dob</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Email</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Gender</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Address</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Phone number</td>
-    <td><input type="text"></input></td>
-  </tr>
-  <tr>
-  <td>Department</td>
-    <td><input type="text"></input></td>
-  </tr>
-</table>
-                   
-                    
-                   </div>
-                <Footer/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div
+          className="app"
+          style={{
+            backgroundColor: "white",
+            color: "black",
+            padding: "30px",
+            marginBottom: "40px",
+          }}
+        >
+          <Link to="/logout">Logout</Link>
+          <div className="topnav">
+            <input
+              type="text"
+              placeholder="Search.."
+              style={{ marginBottom: "4px" }}
+            ></input>
+          </div>
+
+          <table id="customers">
+            <tr>
+              <td>Authentication ID</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Employee ID</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>First Name</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Last Name</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Dob</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Address</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Phone number</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Department</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
